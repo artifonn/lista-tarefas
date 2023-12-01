a37734c632040eb754280e32ca7b9838004dc314
Sérgio Artifon <artifonn@gmail.com>
2023-12-01 19:24:26 -0300

    Implementação: Limpa o imput ao adiconior um item na lista

--- src/script.js
+++ src/script.js
@@ -1,14 +1,19 @@
 
 const capturarInput = () => {
-  let input = document.querySelector('input').value;
-  let addItens = document.getElementById('addItens');
-  let novoLi = document.createElement('li');
-  novoLi.innerText = input;
-  addItens.appendChild(novoLi);
+  let input = document.querySelector('input');
+  let inputValue = input.value.trim();
+
+  if(inputValue !== '') {
+    let addItens = document.getElementById('addItens');
+    let novoLi = document.createElement('li');
+    novoLi.innerText = inputValue;
+    addItens.appendChild(novoLi);
+    input.value = "";
+  }
 }
 
 const marcarItem = (event) => {
   if(event.target.tagName === 'LI') {
     event.target.classList.toggle('remove');
   }
-}
+} 
